71c78c4e9a8e3baad44c8471a2ca07e940655a1a
Joann Lai <lai.joann.cy@gmail.com>
2017-03-13 20:06:33 +0000

    Added downloadImageByURL function

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,21 @@
 const request = require('request');
+const https = require('https');
+const fs = require('fs');
 
 const GITHUB_USER = "laijoann";
 const GITHUB_TOKEN = "aaaf82d1dabd9d63b70f5aaa365f5e13dc671b84";
+
+let urlList = [];
 
 callback = (error, response, body) => {
   if (error) console.error(error);
   else if (response.statusCode === 200) {
     parsedBody = JSON.parse(body);
-    for (let i = 0; i < parsedBody.length; i++) {
-      console.log(parsedBody[i].avatar_url);
+    for (contrib in parsedBody) {
+      urlList.push(parsedBody[contrib].avatar_url);
     }
+    downloadImageByURL(urlList);
+    //TODO: but is this abiding by async?
   }
 }
 
@@ -25,3 +31,23 @@ getRepoContributors = (repoOwner, repoName, cb) => {
 }
 
 getRepoContributors("jquery", "jquery", callback);
+
+downloadImageByURL = (urlList) => {
+  for (contrib in urlList) {
+    request.get(urlList[contrib])
+    .on('error', (err) => {
+      console.error(err);
+    })
+    .pipe(fs.createWriteStream('./avatarGallery/' + contrib + '.jpg'))
+    .on('finish', () => {
+      console.log('Avatar download complete.');
+    })
+  }
+}
+
+
+
+
+
+
+
