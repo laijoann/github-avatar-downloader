eb01cd60a53bd7cda6c448b2fa48b572e0db7742
Joann Lai <lai.joann.cy@gmail.com>
2017-03-13 19:28:35 +0000

    Set up getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -3,22 +3,20 @@ const request = require('request');
 const GITHUB_USER = "laijoann";
 const GITHUB_TOKEN = "aaaf82d1dabd9d63b70f5aaa365f5e13dc671b84"; //is this supposed to be a string or a number
 
-let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + "jquery" + '/' + "jquery" + '/contributors'; //replace the "jqery"s with repoOwner, and repoName getRepoContributors
-
-let options = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'Github Avator Downloader'
-  }
-};
-
 callback = (error, response, body) => {
   if (error) console.error(error);
   console.log(response.statusCode);
 }
 
-request(options, callback);
+getRepoContributors = (repoOwner, repoName, cb) => {
+  let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  let options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Github Avator Downloader'
+    }
+  };
+  request(options, cb);
+}
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   ...
-// }
+getRepoContributors("jquery", "jquery", callback);
